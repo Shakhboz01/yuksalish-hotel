b9a4f1248cb495b5b7b6140370070fc69dadb26f
Shakhboz <shfazliddinov1940@gmail.com>
2023-10-14 15:51:29 +0500

    made changes to user url

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root "pages#main_page"
   devise_for :users
 
-  resources :users do
+  resources :users, except: %i[update] do
+    post :update, on: :member
     get :toggle_active_user, on: :member
     get :new_user_form, on: :collection
     post :auto_user_creation, on: :collection
