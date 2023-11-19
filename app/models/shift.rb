ede6c4ea445762e56d6f07a2b60bbb97168e5e2d
Shakhboz <shfazliddinov1940@gmail.com>
2023-11-19 22:13:27 +0500

    change to shift

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,7 +2,7 @@ class Shift < ApplicationRecord
   belongs_to :user
   before_create :check_unclosed_shifts
   after_create :send_creation_message
-  after_update :send_closed_message, if: :saved_changed_to_closed_at
+  before_update :send_closed_message, if: :saved_changed_to_closed_at
 
   scope :unclosed, -> { where(closed_at: nil) }
 
@@ -20,6 +20,10 @@ class Shift < ApplicationRecord
   end
 
   def send_closed_message
+    expenditures = Expenditure.where("created_at > ?", created_at).sum(:price)
+    incomes = TopUp.where("created_at > ?", created_at).sum(:price)
+    self.total_expenditure = expenditures
+    self.total_income = incomes
     SendMessage.run(message: "<h3>Смена закрыт</h3>\n" \
                              "Итого приход: 0 сум \n" \
                              "Итого расход: 0 сум")
