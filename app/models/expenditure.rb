392ed36e59c7a9e91f23df9b2007a755295cbd59
yuk_hotel <hotel@gmail.com>
2024-01-07 16:43:20 +0500

    added changes

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -2,5 +2,5 @@ class Expenditure < ApplicationRecord
   include ShiftIsPresent
   belongs_to :user
   validates :price, comparison: { greater_than: 0 }
-  enum expenditure_type: %i[на_завтрак зарплата аванс коммунальная_услуга откат магазин другие]
+  enum expenditure_type: %i[зарплата аванс коммунальная_услуга откат магазин другие]
 end
